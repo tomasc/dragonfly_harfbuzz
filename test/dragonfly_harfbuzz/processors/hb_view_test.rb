056fc7315e2c44d2b6c9a29715c654f03e5da184
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-27 22:13:20 +0200

    better test reports

--- test/dragonfly_harfbuzz/processors/hb_view_test.rb
+++ test/dragonfly_harfbuzz/processors/hb_view_test.rb
@@ -7,28 +7,32 @@ describe DragonflyHarfbuzz::Processors::HbView do
 
   let(:string) { 'FOO' }
 
-  DragonflyHarfbuzz::SUPPORTED_FORMATS.each do |format|
-    unless File.exists?(SAMPLES_DIR.join("sample.#{format}"))
-      it(format) { skip "sample.#{format} does not exist, skipping" }
-      next
-    end
-    
-    describe format.to_s do
+  describe 'SUPPORTED_FORMATS' do
+    DragonflyHarfbuzz::SUPPORTED_FORMATS.each do |format|
+      unless File.exists?(SAMPLES_DIR.join("sample.#{format}"))
+        it(format) { skip "sample.#{format} does not exist, skipping" }
+        next
+      end
+
       let(:content) { app.fetch_file SAMPLES_DIR.join("sample.#{format}") }
-      let(:result) { content.hb_view(string) }
-      it { result.ext.must_equal 'svg' }
-      it { result.mime_type.must_equal 'image/svg+xml' }
-      it { result.size.must_be :>, 0 }
+      it(format) do
+        result = content.hb_view(string)
+        result.ext.must_equal 'svg'
+        result.mime_type.must_equal 'image/svg+xml'
+        result.size.must_be :>, 0
+      end
     end
   end
 
-  DragonflyHarfbuzz::SUPPORTED_OUTPUT_FORMATS.each do |format|
-    describe "output to #{format}" do
+  describe 'SUPPORTED_OUTPUT_FORMATS' do
+    DragonflyHarfbuzz::SUPPORTED_OUTPUT_FORMATS.each do |format|
       let(:content) { app.fetch_file SAMPLES_DIR.join("sample.otf") }
-      let(:result) { content.hb_view(string, format: format) }
-      it { result.ext.must_equal format }
-      it { result.mime_type.must_equal Rack::Mime.mime_type(".#{format}") }
-      it { result.size.must_be :>, 0 }
+      it(format) do
+        result = content.hb_view(string, format: format)
+        result.ext.must_equal format
+        result.mime_type.must_equal Rack::Mime.mime_type(".#{format}")
+        result.size.must_be :>, 0
+      end
     end
   end
 
