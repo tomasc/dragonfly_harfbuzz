21c5573f2f1c9ca17353ff01b35004d9c2deafe4
Tomáš Celizna <tomas.celizna@gmail.com>
2018-08-03 09:40:44 +0200

    cleanup

--- test/dragonfly_harfbuzz/flatten_svg_service_test.rb
+++ test/dragonfly_harfbuzz/flatten_svg_service_test.rb
@@ -9,26 +9,24 @@ module DragonflyHarfbuzz
     let(:svg) { processor.call(content, string) }
     let(:call) { DragonflyHarfbuzz::FlattenSvgService.call(string, svg) }
 
-    describe 'flatten_svg' do
-      before { processor.call(content, string, flatten_svg: true) }
+    before { processor.call(content, string, flatten_svg: true) }
 
-      describe 'lines' do
-        it { content.data.must_include "line=\"#{string}\" class=\"line\"" }
-      end
+    describe 'lines' do
+      it { content.data.must_include "line=\"#{string}\" class=\"line\"" }
+    end
 
-      describe 'words' do
-        it { content.data.must_include '<g word="ABC" class="word">' }
-        it { content.data.must_include '<g word="def" class="word">' }
-      end
+    describe 'words' do
+      it { content.data.must_include '<g word="ABC" class="word">' }
+      it { content.data.must_include '<g word="def" class="word">' }
+    end
 
-      describe 'characters' do
-        it { content.data.must_include '<svg character="A" class="character"' }
-        it { content.data.must_include '<svg character="B" class="character"' }
-        it { content.data.must_include '<svg character="C" class="character"' }
-        it { content.data.must_include '<svg character="d" class="character"' }
-        it { content.data.must_include '<svg character="e" class="character"' }
-        it { content.data.must_include '<svg character="f" class="character"' }
-      end
+    describe 'characters' do
+      it { content.data.must_include '<svg character="A" class="character"' }
+      it { content.data.must_include '<svg character="B" class="character"' }
+      it { content.data.must_include '<svg character="C" class="character"' }
+      it { content.data.must_include '<svg character="d" class="character"' }
+      it { content.data.must_include '<svg character="e" class="character"' }
+      it { content.data.must_include '<svg character="f" class="character"' }
     end
   end
 end
