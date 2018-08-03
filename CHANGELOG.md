72bd0e9c3ace6374c2269801883b3b1f30572f7a
Tomáš Celizna <tomas.celizna@gmail.com>
2018-08-03 11:14:09 +0200

    release 1.1.0
    * support `unicodes` option for `markup_svg: true`
    * `split_paths: false` by default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## 1.1.0
+
+* support `unicodes` option for `markup_svg: true`
+* `split_paths: false` by default
+
 ## 1.0.0
 
 * add `SUPPORTED_FORMATS` and `SUPPORTED_OUTPUT_FORMATS` and raise errors when formats are not matching
--- lib/dragonfly_harfbuzz/processors/hb_view.rb
+++ lib/dragonfly_harfbuzz/processors/hb_view.rb
@@ -5,6 +5,7 @@ module DragonflyHarfbuzz
     class HbView
       def call(content, str, options = {})
         options = options.each_with_object({}) { |(k, v), memo| memo[k.to_s] = v } # stringify keys
+        unicodes = options.fetch('unicodes', nil)
 
         raise UnsupportedFormat unless SUPPORTED_FORMATS.include?(content.ext)
 
@@ -14,7 +15,7 @@ module DragonflyHarfbuzz
 
         flatten_svg = options.fetch('flatten_svg', false)
         markup_svg = options.fetch('markup_svg', flatten_svg)
-        split_paths = options.fetch('split_paths', true)
+        split_paths = options.fetch('split_paths', false)
 
         content.shell_update(ext: format) do |old_path, new_path|
           args = %W[
@@ -36,7 +37,7 @@ module DragonflyHarfbuzz
         content.ext = format
 
         if format =~ /svg/i
-          content.update(MarkupSvgService.call(str, content.data, split_paths: split_paths), 'name' => "temp.#{format}") if markup_svg
+          content.update(MarkupSvgService.call((str || str_from_unicodes(unicodes)), content.data, split_paths: split_paths), 'name' => "temp.#{format}") if markup_svg
           content.update(FlattenSvgService.call(content.data), 'name' => "temp.#{format}") if flatten_svg
           content.update(DomAttrsService.call(content.data, options[:font_size], options[:margin]), 'name' => "temp.#{format}")
         end
@@ -48,6 +49,12 @@ module DragonflyHarfbuzz
       end
 
       private
+
+      def str_from_unicodes(unicodes)
+        unicodes.split(",").map do |hexstring|
+          Array(hexstring).pack("H*").force_encoding('utf-16be').encode('utf-8')
+        end.join
+      end
 
       def hb_view_command
         'hb-view'
