1cbda1a95189e697fb2457c92cfd4bbcd2228742
Tomáš Celizna <tomas.celizna@gmail.com>
2018-08-03 09:38:00 +0200

    improve tests

--- test/dragonfly_harfbuzz/flatten_svg_service_test.rb
+++ test/dragonfly_harfbuzz/flatten_svg_service_test.rb
@@ -5,15 +5,26 @@ module DragonflyHarfbuzz
     let(:app) { test_app.configure_with(:harfbuzz) }
     let(:processor) { DragonflyHarfbuzz::Processors::HbView.new }
     let(:content) { Dragonfly::Content.new(app, SAMPLES_DIR.join('sample.otf')) }
-    let(:string) { 'foo     foo' }
+    let(:string) { 'ABC     def' }
     let(:svg) { processor.call(content, string) }
     let(:call) { DragonflyHarfbuzz::FlattenSvgService.call(string, svg) }
 
-    let(:f_markup) { '<svg character="f" class="character" overflow="visible" x="1552" y="229.504">' }
-
     describe 'flatten_svg' do
       before { processor.call(content, string, flatten_svg: true) }
-      it { content.data.sub(f_markup, '').wont_include f_markup }
+
+      describe 'words' do
+        it { content.data.must_include '<g word="ABC" class="word">' }
+        it { content.data.must_include '<g word="def" class="word">' }
+      end
+
+      describe 'characters' do
+        it { content.data.must_include '<svg character="A" class="character"' }
+        it { content.data.must_include '<svg character="B" class="character"' }
+        it { content.data.must_include '<svg character="C" class="character"' }
+        it { content.data.must_include '<svg character="d" class="character"' }
+        it { content.data.must_include '<svg character="e" class="character"' }
+        it { content.data.must_include '<svg character="f" class="character"' }
+      end
     end
   end
 end
